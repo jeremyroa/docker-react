058c5afb934288cbe6209b7ff63ea70771de17b2
jeremy <jmra0611@gmail.com>
2020-04-05 13:01:07 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ss <code>src/App.js</code> and save to reload.
+          Learning React with docker and CI <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
